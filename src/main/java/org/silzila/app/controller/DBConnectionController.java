3c5038845ddd4e29568ee3096c2fd7af4242a8af
silzila <mail@thebalu.com>
2022-08-21 23:08:41 +0530

    API - DB Connection CRUD Service call code is split into dedicated Service Class

--- src/main/java/org/silzila/app/controller/DBConnectionController.java
+++ src/main/java/org/silzila/app/controller/DBConnectionController.java
@@ -1,12 +1,15 @@
 package org.silzila.app.controller;
 
 import org.modelmapper.ModelMapper;
-import org.silzila.app.converter.DBConnectionConverter;
+// import org.silzila.app.converter.DBConnectionConverter;
 import org.silzila.app.dto.DBConnectionDTO;
+import org.silzila.app.exception.BadRequestException;
+import org.silzila.app.exception.RecordNotFoundException;
 import org.silzila.app.model.DBConnection;
 import org.silzila.app.payload.request.DBConnectionRequest;
 import org.silzila.app.payload.response.MessageResponse;
 import org.silzila.app.repository.DBConnectionRepository;
+import org.silzila.app.security.service.DBConnectionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,7 +35,10 @@ public class DBConnectionController {
     DBConnectionRepository dbConnectionRepository;
 
     @Autowired
-    DBConnectionConverter dbConnectionConverter;
+    DBConnectionService dbConnectionService;
+
+    // @Autowired
+    // DBConnectionConverter dbConnectionConverter;
 
     ModelMapper mapper = new ModelMapper();
 
@@ -45,104 +51,54 @@ public class DBConnectionController {
 
     @PostMapping("/database-connection")
     public ResponseEntity<?> registerDBConnection(@RequestHeader Map<String, String> reqHeader,
-            @Valid @RequestBody DBConnectionRequest dbConnectionRequest) {
+            @Valid @RequestBody DBConnectionRequest dbConnectionRequest) throws BadRequestException {
         // get the rquester user id
         String userId = reqHeader.get("requesterUserId");
-        // check if connection name is alredy used for the requester
-        List<DBConnection> connection_list = dbConnectionRepository.findByUserIdAndConnectionName(userId,
-                dbConnectionRequest.getConnectionName());
-        // if connection name is alredy used, send error
-        if (!connection_list.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new MessageResponse("Error: Connection Name is already taken!"));
-        }
-        // create DB Connection object and save it to DB
-        DBConnection dbConnection = new DBConnection(
-                userId,
-                dbConnectionRequest.getVendor(),
-                dbConnectionRequest.getServer(),
-                dbConnectionRequest.getPort(),
-                dbConnectionRequest.getDatabase(),
-                dbConnectionRequest.getUsername(),
-                dbConnectionRequest.getPassword(),
-                dbConnectionRequest.getConnectionName());
-        dbConnectionRepository.save(dbConnection);
-        return ResponseEntity.status(HttpStatus.OK).body(new MessageResponse("new DB Connection registered!"));
+        // make service call to add record
+        DBConnectionDTO dto = dbConnectionService.createDBConnection(dbConnectionRequest, userId);
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping("/database-connection")
-    public ResponseEntity<?> getAllDBConnection(@RequestHeader Map<String, String> reqHeader) {
+    public ResponseEntity<?> getAllDBConnections(@RequestHeader Map<String, String> reqHeader) {
         // get the rquester user id
         String userId = reqHeader.get("requesterUserId");
-        List<DBConnectionDTO> dtos = new ArrayList<>();
-        // fetch all DB connections for the user
-        List<DBConnection> dbConnections = dbConnectionRepository.findByUserId(userId);
-        // convert to DTO object to not show Password
-        dbConnections.forEach(dbconnection -> dtos.add(mapper.map(dbconnection,
-                DBConnectionDTO.class)));
+        // service call to get list of DB connections,
+        // empty list will not throw exceptions but return as empty list
+        List<DBConnectionDTO> dtos = dbConnectionService.getAllDBConnections(userId);
         return ResponseEntity.ok(dtos);
     }
 
     @GetMapping("/database-connection/{id}")
-    public ResponseEntity<?> getDBConnection(@RequestHeader Map<String, String> reqHeader,
-            @PathVariable(value = "id") String id) {
+    public ResponseEntity<?> getConnectionById(@RequestHeader Map<String, String> reqHeader,
+            @PathVariable(value = "id") String id) throws RecordNotFoundException {
         // get the rquester user id
         String userId = reqHeader.get("requesterUserId");
-        // fetch the particular DB connection for the user
-        Optional<DBConnection> optionalDBConnection = dbConnectionRepository.findByIdAndUserId(id, userId);
-        // if no connection details, then send NOT FOUND Error
-        if (!optionalDBConnection.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new MessageResponse("Error: DB Connection Id not exists"));
-        }
-        DBConnection dbConnection = optionalDBConnection.get();
-        // convert to DTO object to not show Password
-        DBConnectionDTO dto = mapper.map(dbConnection, DBConnectionDTO.class);
+        // service call to get the DB Connection details
+        DBConnectionDTO dto = dbConnectionService.getDBConnectionById(id, userId);
         return ResponseEntity.ok(dto);
 
     }
 
     @PutMapping("/database-connection/{id}")
     public ResponseEntity<?> updateDBConnection(@RequestHeader Map<String, String> reqHeader,
-            @Valid @RequestBody DBConnectionRequest dbConnectionRequest, @PathVariable(value = "id") String id) {
+            @Valid @RequestBody DBConnectionRequest dbConnectionRequest, @PathVariable(value = "id") String id)
+            throws RecordNotFoundException, BadRequestException {
         String userId = reqHeader.get("requesterUserId");
-        Optional<DBConnection> dbConnection = dbConnectionRepository.findById(id);
-        if (!dbConnection.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new MessageResponse("Error: DB Connection Id not exists"));
-        }
-        List<DBConnection> dbConnections = dbConnectionRepository
-                .findByIdNotAndUserIdAndConnectionName(
-                        id, userId, dbConnectionRequest.getConnectionName());
-        if (!dbConnections.isEmpty()) {
-            return ResponseEntity.badRequest().body(new MessageResponse("Error: Connection Name is already taken!"));
-        }
-        DBConnection _dbConnection = dbConnection.get();
-        _dbConnection.setConnectionName(dbConnectionRequest.getConnectionName());
-        _dbConnection.setVendor(dbConnectionRequest.getVendor());
-        _dbConnection.setServer(dbConnectionRequest.getServer());
-        _dbConnection.setPort(dbConnectionRequest.getPort());
-        _dbConnection.setDatabase(dbConnectionRequest.getDatabase());
-        _dbConnection.setUsername(dbConnectionRequest.getUsername());
-        _dbConnection.setPassword(dbConnectionRequest.getDatabase());
-        return new ResponseEntity<>(dbConnectionRepository.save(_dbConnection), HttpStatus.OK);
-
+        // service call to update
+        DBConnectionDTO dto = dbConnectionService.updateDBConnection(id, dbConnectionRequest, userId);
+        return ResponseEntity.ok(dto);
     }
 
     @DeleteMapping("/database-connection/{id}")
     public ResponseEntity<?> deleteDBConnection(@RequestHeader Map<String, String> reqHeader,
-            @PathVariable(value = "id") String id) {
+            @PathVariable(value = "id") String id) throws RecordNotFoundException {
         // get the rquester user id
         String userId = reqHeader.get("requesterUserId");
-        // first check if the connection details present for the id
-        Optional<DBConnection> optionalDBConnection = dbConnectionRepository.findByIdAndUserId(id, userId);
-        if (!optionalDBConnection.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new MessageResponse("Error: DB Connection Id not exists"));
-        }
-        // delete the connection details row from database
-        dbConnectionRepository.deleteById(id);
+        // service call to delete
+        dbConnectionService.deleteDBConnection(id, userId);
         return ResponseEntity.ok().body(new MessageResponse("DB Connection is deleted!"));
+
     }
 
 }
--- src/main/java/org/silzila/app/security/service/DBConnectionService.java
+++ src/main/java/org/silzila/app/security/service/DBConnectionService.java
@@ -1,5 +1,113 @@
 package org.silzila.app.security.service;
 
+import org.silzila.app.repository.DBConnectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.silzila.app.dto.DBConnectionDTO;
+import org.silzila.app.model.DBConnection;
+import org.silzila.app.payload.request.DBConnectionRequest;
+import org.silzila.app.exception.RecordNotFoundException;
+import org.silzila.app.exception.BadRequestException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.modelmapper.ModelMapper;
+
+@Service
 public class DBConnectionService {
+
+    @Autowired
+    DBConnectionRepository dbConnectionRepository;
+
+    ModelMapper mapper = new ModelMapper();
+
+    public List<DBConnectionDTO> getAllDBConnections(String userId) {
+        // fetch all DB connections for the user
+        List<DBConnection> dbConnections = dbConnectionRepository.findByUserId(userId);
+        // convert to DTO object to not show Password
+        List<DBConnectionDTO> dtos = new ArrayList<>();
+        dbConnections.forEach(dbconnection -> dtos.add(mapper.map(dbconnection,
+                DBConnectionDTO.class)));
+        return dtos;
+    }
+
+    public DBConnectionDTO getDBConnectionById(String id, String userId)
+            throws RecordNotFoundException {
+        // fetch the particular DB connection for the user
+        Optional<DBConnection> optionalDBConnection = dbConnectionRepository.findByIdAndUserId(id, userId);
+        // if no connection details, then send NOT FOUND Error
+        if (!optionalDBConnection.isPresent()) {
+            throw new RecordNotFoundException("Error: No such Connection Id exists");
+        }
+        DBConnection dbConnection = optionalDBConnection.get();
+        // convert to DTO object to not show Password
+        DBConnectionDTO dto = mapper.map(dbConnection, DBConnectionDTO.class);
+        return dto;
+    }
+
+    public DBConnectionDTO createDBConnection(DBConnectionRequest dbConnectionRequest, String userId)
+            throws BadRequestException {
+        // check if connection name is alredy used for the requester
+        List<DBConnection> connection_list = dbConnectionRepository.findByUserIdAndConnectionName(userId,
+                dbConnectionRequest.getConnectionName());
+        // if connection name is alredy used, send error
+        if (!connection_list.isEmpty()) {
+            throw new BadRequestException("Error: Connection Name is already taken!");
+        }
+        // create DB Connection object and save it to DB
+        DBConnection dbConnection = new DBConnection(
+                userId,
+                dbConnectionRequest.getVendor(),
+                dbConnectionRequest.getServer(),
+                dbConnectionRequest.getPort(),
+                dbConnectionRequest.getDatabase(),
+                dbConnectionRequest.getUsername(),
+                dbConnectionRequest.getPassword(),
+                dbConnectionRequest.getConnectionName());
+        dbConnectionRepository.save(dbConnection);
+        DBConnectionDTO dto = mapper.map(dbConnection, DBConnectionDTO.class);
+        return dto;
+    }
+
+    public DBConnectionDTO updateDBConnection(String id, DBConnectionRequest dbConnectionRequest, String userId)
+            throws RecordNotFoundException, BadRequestException {
+        // fetch the particular DB connection for the user
+        Optional<DBConnection> optionalDBConnection = dbConnectionRepository.findByIdAndUserId(id, userId);
+        // if no connection details, then send NOT FOUND Error
+        if (!optionalDBConnection.isPresent()) {
+            throw new RecordNotFoundException("Error: No such Connection Id exists");
+        }
+        List<DBConnection> dbConnections = dbConnectionRepository
+                .findByIdNotAndUserIdAndConnectionName(
+                        id, userId, dbConnectionRequest.getConnectionName());
+        if (!dbConnections.isEmpty()) {
+            throw new BadRequestException("Error: Connection Name is alread taken!");
+        }
+        DBConnection _dbConnection = optionalDBConnection.get();
+        _dbConnection.setConnectionName(dbConnectionRequest.getConnectionName());
+        _dbConnection.setVendor(dbConnectionRequest.getVendor());
+        _dbConnection.setServer(dbConnectionRequest.getServer());
+        _dbConnection.setPort(dbConnectionRequest.getPort());
+        _dbConnection.setDatabase(dbConnectionRequest.getDatabase());
+        _dbConnection.setUsername(dbConnectionRequest.getUsername());
+        _dbConnection.setPassword(dbConnectionRequest.getDatabase());
+        dbConnectionRepository.save(_dbConnection);
+        DBConnectionDTO dto = mapper.map(_dbConnection, DBConnectionDTO.class);
+        return dto;
+    }
+
+    public void deleteDBConnection(String id, String userId)
+            throws RecordNotFoundException {
+        // fetch the particular DB connection for the user
+        Optional<DBConnection> optionalDBConnection = dbConnectionRepository.findByIdAndUserId(id, userId);
+        // if no connection details, then send NOT FOUND Error
+        if (!optionalDBConnection.isPresent()) {
+            throw new RecordNotFoundException("Error: No such Connection Id exists");
+        }
+        // delete the record from DB
+        dbConnectionRepository.deleteById(id);
+    }
 
 }
