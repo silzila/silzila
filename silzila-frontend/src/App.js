b4a8f67a9ec7549ac7164e30183129faab7364e6
prakash-npm <43378891+prakash-npm@users.noreply.github.com>
2022-10-04 18:58:36 +0530

    Initial Commit
    1. App.js modified
    2. Redux Store added

--- silzila-frontend/src/App.js
+++ silzila-frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import allReducers from "./redux";
+
+const middleware = [reduxThunk];
+const store = createStore(allReducers, composeWithDevTools(applyMiddleware(...middleware)));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Silzila UI</h1>
+      </div>
+    </Provider>
   );
 }
 
---
+++ silzila-frontend/src/redux/UserInfo/isLoggedActions.js
@@ -0,0 +1,7 @@
+export const userAuthentication = (payload) => {
+	return { type: "USER_AUTHENTICATED", payload: payload };
+};
+
+export const resetUser = () => {
+	return { type: "RESET_USER" };
+};
