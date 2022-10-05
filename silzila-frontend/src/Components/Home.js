a65aa896543066a86b93c95f460d5de695e1c0de
prakash-npm <43378891+prakash-npm@users.noreply.github.com>
2022-10-05 08:46:53 +0530

    Login Page Added
    1. Login Page
    2. Sign Up Page
    3. Fetch Data

--- silzila-frontend/src/Components/Home.js
+++ silzila-frontend/src/Components/Home.js
@@ -2,24 +2,40 @@ import React from "react";
 import { connect } from "react-redux";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
+
+import Login from "./LoginSignUp/Login";
+import SignUp from "./LoginSignUp/SignUp";
 import Redirect from "./Redirect";
 
 const Home = (props) => {
 	return (
-    // TODO: Priority 10 - Capture Browser's navigation clicks
-    // Pressing back or forward in browser navigation brings to different Pages.
-    // If the required data is not there in the Pages, they crash
-    // Eg. After going to create / edit Dataset page, come back to dataHome
-    // then again click forward to go to editDataset page
+		// TODO: Priority 10 - Capture Browser's navigation clicks
+		// Pressing back or forward in browser navigation brings to different Pages.
+		// If the required data is not there in the Pages, they crash
+		// Eg. After going to create / edit Dataset page, come back to dataHome
+		// then again click forward to go to editDataset page
 
-    <React.Fragment>
-      <Router>
-        <Routes>
-          <Route path="*" element={<Redirect />} />
-        </Routes>
-      </Router>
-    </React.Fragment>
-  );
+		<React.Fragment>
+			{props.isUserLogged ? (
+				<Router>
+					<Routes>
+						<Route exact path="/" element={<Login />} />
+						<Route exact path="/login" element={<Login />} />
+						<Route exact path="/signup" element={<SignUp />} />						
+					</Routes>
+				</Router>
+			) : (
+				<Router>
+					<Routes>
+						<Route path="*" element={<Redirect />} />
+						<Route exact path="/" element={<Login />} />
+						<Route exact path="/login" element={<Login />} />
+						<Route exact path="/signup" element={<SignUp />} />
+					</Routes>
+				</Router>
+			)}
+		</React.Fragment>
+	);
 };
 
 const mapStateToProps = (state) => {
