aabae5b5152ae94e83d7fd5a34e2065303142de4
Jeeva-SJKPY <sjeeva.mail@gmail.com>
2024-06-20 17:20:40 +0530

    fixing data type issue in custom query

--- src/main/java/com/silzila/payload/response/MetadataColumn.java
+++ src/main/java/com/silzila/payload/response/MetadataColumn.java
@@ -18,18 +18,17 @@ public class MetadataColumn {
 
     public MetadataColumn(String columnName, String dataType) {
         this.columnName = columnName;
-
-        if (dataType.matches("4|5|-5|-6")) {
+        if (dataType.matches("4|5|-5|-6|int4|INT64|INT|int|NUMBER|BIGINT")) {
             this.dataType = "integer";
-        } else if (dataType.matches("2|3|6|7|8")) {
+        } else if (dataType.matches("2|3|6|7|8|numeric|FLOAT64|DECIMAL|decimal|NUMBER|DOUBLE")) {
             this.dataType = "decimal";
-        } else if (dataType.matches("-1|1|12|7|8|-9")) {
+        } else if (dataType.matches("-1|1|12|7|8|-9|varchar|STRING|VARCHAR|nvarchar")) {
             this.dataType = "text";
-        } else if (dataType.matches("91")) {
+        } else if (dataType.matches("91|date|DATE")) {
             this.dataType = "date";
-        } else if (dataType.matches("92|93")) {
+        } else if (dataType.matches("92|93|timestamp|TIMESTAMP|datetime2|TIMESTAMPNTZ|DATETIME")) {
             this.dataType = "timestamp";
-        } else if (dataType.matches("0")) {
+        } else if (dataType.matches("0|null|NULL")) {
             this.dataType = "null";
         }
         // else if (dataType.matches("-2|-3|-4")) {
