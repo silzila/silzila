22f1a4fb25fd9f256b5e8057057700efac130f92
Jeeva-SJKPY <sjeeva.mail@gmail.com>
2024-06-23 12:08:30 +0530

    changing datatype to oid

--- src/main/java/com/silzila/payload/response/MetadataColumn.java
+++ src/main/java/com/silzila/payload/response/MetadataColumn.java
@@ -21,28 +21,17 @@ public class MetadataColumn {
 
     public MetadataColumn(String columnName, String dataType) {
         this.columnName = columnName;
-        HashSet<String> integerType = new HashSet<String>(
-                Set.of("4", "5", "-5","-6","int4","INT64","INT","int","NUMBER","BIGINT"));;
-        HashSet<String> decimalType = new HashSet<String>(
-                Set.of("2","3","6","7","8","numeric","FLOAT64","DECIMAL","decimal","DOUBLE","FLOAT"));
-        HashSet<String> textType = new HashSet<String>(
-                Set.of("-1","1","12","7","8","-9","varchar","STRING","VARCHAR","nvarchar","VARCHAR2","NVARCHAR2","NVARCHAR"));
-        HashSet<String> dateType = new HashSet<String>(
-                Set.of("91","date","DATE"));
-        HashSet<String> timestampType = new HashSet<String>(
-                Set.of("92","93","timestamp","TIMESTAMP","datetime2","TIMESTAMPNTZ","DATETIME","DATETIME2","timestamptz"));
-
-        if (integerType.contains(dataType)) {
+        if (dataType.matches("4|5|-5|-6")) {
             this.dataType = "integer";
-        } else if (decimalType.contains(dataType)) {
+        } else if (dataType.matches("2|3|6|7|8")) {
             this.dataType = "decimal";
-        } else if (textType.contains(dataType)) {
+        } else if (dataType.matches("-1|1|12|-9")) {
             this.dataType = "text";
-        } else if (dateType.contains(dataType)) {
+        } else if (dataType.matches("91")) {
             this.dataType = "date";
-        } else if (timestampType.contains(dataType)) {
+        } else if (dataType.matches("92|93")) {
             this.dataType = "timestamp";
-        } else if (dataType.matches("0|null|NULL")) {
+        } else if (dataType.matches("0")) {
             this.dataType = "null";
         }
         // else if (dataType.matches("-2|-3|-4")) {
