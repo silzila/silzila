08895fd42ed9038ccdc6ae622c81df4dcc5acf29
Saropalani <nive58palani@gmail.com>
2024-08-22 14:05:41 +0530

    changed close icon position in the tab and tile component.

--- silzila-frontend/src/Components/DataConnection/FlatFileUpload.tsx
+++ silzila-frontend/src/Components/DataConnection/FlatFileUpload.tsx
@@ -59,6 +59,7 @@ const handleFileSelect = (file: File | undefined) => {
       ) {
           setErrorDialogMessage(`Invalid file type. Expected a ${selectedFileType.toUpperCase()} file.`);
           setErrorDialogOpen(true);
+          setSelectedFile
           return;
       } 
       if (selectedFileType === 'excel' && fileExtension !== 'xlsx') {
