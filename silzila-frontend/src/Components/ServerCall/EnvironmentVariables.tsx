bdd35b3208d6e6b586bb2ab1f3549b0fea6206ad
Saropalani <nive58palani@gmail.com>
2025-01-21 14:16:41 +0530

    Login and signup components modified.

--- silzila-frontend/src/Components/ServerCall/EnvironmentVariables.tsx
+++ silzila-frontend/src/Components/ServerCall/EnvironmentVariables.tsx
@@ -3,6 +3,7 @@
 
 // for SERVER+LOCAL TWO TIER - separate front end & backend
 export const serverEndPoint = "https://bi.silzila.com/api/";
+export const localEndPoint = "http://localhost:3000/";
 // for LOCAL-ONLY SINGLE TIER - single build with react app
 // export const serverEndPoint = "http://localhost:8080/";
 
