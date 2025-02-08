7481daec3d712141bea62a316d127667bee891fb
DarkTHor55 <negir166661@gmail.com>
2025-02-08 11:48:19 +0530

    added a import  for MultipartFile

--- src/main/java/com/silzila/service/FileDataService.java
+++ src/main/java/com/silzila/service/FileDataService.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
