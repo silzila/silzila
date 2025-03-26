d4fb4926bf2502eb2ece7d4d4d468886ed0c6e2f
Abinaya311 <abinaya03cse@gmail.com>
2025-03-26 16:03:41 +0530

    proper error message occurs for flatfiles

--- silzila-frontend/src/Components/DataConnection/FlatFileUpload.tsx
+++ silzila-frontend/src/Components/DataConnection/FlatFileUpload.tsx
@@ -11,10 +11,9 @@ import {
   Select,
   Typography,
 } from "@mui/material";
-import React, { useCallback, useState } from "react";
+import { useCallback, useState } from "react";
 import { connect } from "react-redux";
-import { useNavigate,  useParams } from "react-router-dom";
-
+import { useNavigate, useParams } from "react-router-dom";
 import { Dispatch } from "redux";
 import {
   setApiResponse,
@@ -27,14 +26,14 @@ import FileDropZone from "./FileDropZone";
 import { FlatFileUploadProps } from "./FlatFileInterfaces";
 import Logger from "../../Logger";
 import MenuBar from "../DataViewer/MenuBar";
+import { fontSize, palette } from "../..";
+import { jwtDecode } from "jwt-decode";
 import jsonFileIcon from "../../assets/jsonFileIcon.png";
 import csvFileIcon from "../../assets/csvFileIcon.png";
 import xlsxFileIcon from "../../assets/xlsxFileIcon.png";
-import { jwtDecode } from "jwt-decode";
-import { fontSize, palette } from "../..";
-import { PopUpSpinner } from "../CommonFunctions/DialogComponents";
 import HoverButton from "../Buttons/HoverButton";
 import { ffButtonStyle } from "./muiStyles";
+import { PopUpSpinner } from "../CommonFunctions/DialogComponents";
 
 const FlatFileUpload = ({
   token,
@@ -46,22 +45,24 @@ const FlatFileUpload = ({
   const [selectedFileType, setSelectedFileType] = useState<
     string | undefined
   >(); // Filetype state
-  // const [selectedSheetName, setSelectedSheetName] = useState<string>(""); // Sheetname state
+  const [selectedSheetName, setSelectedSheetName] = useState<string>(""); // Sheetname state
+
   const { parentId } = useParams();
 
-  // Error dialog state
-  const [errorDialogOpen, setErrorDialogOpen] = useState(false);
-  const [errorDialogMessage, setErrorDialogMessage] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [sheetChooseDialogShow, setSheetChooseDialogShow] = useState(false);
-  const [sheetNames, setSheetNames] = useState<string[]>([]);
-  const [sheetLoading, setSheetLoading] = useState(false);
-  const [fileInfo, setFileInfo] = useState<{fileId:string,
-    fileName:string,
+  const [fileInfo, setFileInfo] = useState<{
+    fileId: string;
+    fileName: string;
   }>({
     fileId: "",
     fileName: "",
   });
+  const [loading, setLoading] = useState(false);
+  const [sheetChooseDialogShow, setSheetChooseDialogShow] = useState(false);
+  const [sheetNames, setSheetNames] = useState<string[]>([]);
+  const [sheetLoading, setSheetLoading] = useState(false);
+  // Error dialog state
+  const [errorDialogOpen, setErrorDialogOpen] = useState(false);
+  const [errorDialogMessage, setErrorDialogMessage] = useState("");
   const [sheetError, setSheetError] = useState<{
     isError: boolean;
     message: string;
@@ -71,6 +72,7 @@ const FlatFileUpload = ({
     fileName: string;
     sheetNames: string[];
   };
+
   const setDataToEditApiResponse = useCallback(
     (data: any) => {
       const fileObj = {
@@ -80,13 +82,12 @@ const FlatFileUpload = ({
         timestampFormat: data.timestampFormat,
         columnInfos: data.columnInfos,
         sampleRecords: data.sampleRecords,
-        fileType: selectedFileType, 
+        fileType: selectedFileType,
       };
       setEditApiResponse(fileObj);
     },
-    [selectedFileType, setEditApiResponse] 
+    [selectedFileType, setEditApiResponse]
   );
-  
 
   // Function to handle file type selection
   const handleFileType = (fileType: string) => {
@@ -95,58 +96,54 @@ const FlatFileUpload = ({
     // setSelectedSheetName(""); // reset sheet name when file type change
     // setSelectedSheet(""); // reset sheet name when file type change
   };
-  // const handleSheetChange = useCallback((newSheet:string) => {
-  //   setSelectedSheet(newSheet);
-  // }, []);
-  // const handleDialogClose = useCallback((val:boolean) => {
-  //   setSheetChooseDialogShow(val);
-  // }, []);
+
   const decodedToken: any = jwtDecode(token);
   const access = decodedToken.access;
 
-  const MAX_FILE_SIZE_MB = access === "community" ? 50 : 100; // File size limit /*Business User limit -100MB Community User - 50MB*/
+  const MAX_FILE_SIZE_MB = 1024 // File size limit 1GB
 
-  
-  
-  
-  const SheetDialog = ({sheetChooseDialogShow, setSheetChooseDialogShow }: {
-      sheetChooseDialogShow: boolean,
-      setSheetChooseDialogShow: React.Dispatch<React.SetStateAction<boolean>>,
-    }) => {
-      const [selectedSheet, setSelectedSheet] = useState<string>("");
-      const uploadExcelFile = async () => {
-        const formData = new FormData();
-        formData.append("fileId", fileInfo.fileId);
-        formData.append("sheetName", selectedSheet);
-        formData.append("fileName", fileInfo.fileName);
-        setLoading(true);
-        const res = await FetchData({
-          requestType: "withData",
-          method: "POST",
-          url: "file-upload-excel",
-          data: formData,
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "multipart/form-data",
-          },
+  const SheetDialog = ({
+    sheetChooseDialogShow,
+    setSheetChooseDialogShow,
+  }: {
+    sheetChooseDialogShow: boolean;
+    setSheetChooseDialogShow: React.Dispatch<React.SetStateAction<boolean>>;
+  }) => {
+    const [selectedSheet, setSelectedSheet] = useState<string>("");
+    const uploadExcelFile = async () => {
+      const formData = new FormData();
+      formData.append("fileId", fileInfo.fileId);
+      formData.append("sheetName", selectedSheet);
+      formData.append("fileName", fileInfo.fileName);
+      setLoading(true);
+      const res = await FetchData({
+        requestType: "withData",
+        method: "POST",
+        url: "file-upload-excel",
+        data: formData,
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log(res);
+      if (res.status) {
+        setApiResponse(res.data);
+        setDataToEditApiResponse(res.data);
+        setLoading(false);
+        navigate(`/editflatfile/${parentId}`, {
+          state: { mode: "New", parentId: parentId },
         });
-        if (res.status) {
-          setApiResponse(res.data);
-          setDataToEditApiResponse(res.data);
-          setLoading(false);
-          navigate(`/editflatfile/${parentId}`, {
-            state: { mode: "New", parentId: parentId },
-          });
-        } else {
-          setLoading(false);
-          Logger("info", "error");
-          setErrorDialogMessage("Error uploading the file");
-          setErrorDialogOpen(true);
-          setSelectedFile(undefined); // reset selectedFile after error
-          // setSelectedSheetName("");
-          setSelectedSheet("");
-        }
-      };
+      } else {
+        setLoading(false);
+        Logger("info", "error");
+        setErrorDialogMessage("Error uploading the file");
+        setErrorDialogOpen(true);
+        setSelectedFile(undefined); // reset selectedFile after error
+        // setSelectedSheetName("");
+        setSelectedSheet("");
+      }
+    };
 
     return (
       <Dialog
@@ -159,17 +156,18 @@ const FlatFileUpload = ({
           justifyContent: "center",
           height: "100%",
           width: "100%",
-          background:"transparent",
+          background: "transparent",
         }}
-        
         TransitionProps={{ timeout: 0 }}
         // onClose={() => setSheetChooseDialogShow(false)}
       >
-        <Box sx={{
-          height:'20rem',
-          width:'20rem',
-          padding:'1rem',
-        }}>
+        <Box
+          sx={{
+            height: "20rem",
+            width: "20rem",
+            padding: "1rem",
+          }}
+        >
           {sheetLoading ? (
             <div
               className="loading-container"
@@ -185,42 +183,50 @@ const FlatFileUpload = ({
               <div className="user-spinner"></div>
             </div>
           ) : sheetError.isError ? (
-            <Box sx={{
-              width:'100%'
-            }}>
-              <Typography sx={{
-                color:'red',
-              }}>
+            <Box
+              sx={{
+                width: "100%",
+              }}
+            >
+              <Typography
+                sx={{
+                  color: "red",
+                }}
+              >
                 Some Error occored while fetching sheet names
               </Typography>
             </Box>
           ) : (
-            <Box
-            >
+            <Box>
               <DialogTitle
                 id="alert-dialog-title"
-                sx={{ fontSize: fontSize.large ,fontWeight:'bold',
-                  paddingInline:0,}}
+                sx={{
+                  fontSize: fontSize.large,
+                  fontWeight: "bold",
+                  paddingInline: 0,
+                }}
               >
                 Select Sheet
               </DialogTitle>
               <Box>
-              <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Sheet name</InputLabel>
-                <Select
-                  value={selectedSheet}
-                  labelId="demo-simple-select-label"
-                  onChange={(e) => setSelectedSheet(e.target.value)}
-                  label="Sheet name"
-                  sx={{ width: "100%" }}
-                >
-                  {sheetNames.map((sheetName) => (
-                    <MenuItem key={sheetName} value={sheetName} >
-                      {sheetName}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">
+                    Sheet name
+                  </InputLabel>
+                  <Select
+                    value={selectedSheet}
+                    labelId="demo-simple-select-label"
+                    onChange={(e) => setSelectedSheet(e.target.value)}
+                    label="Sheet name"
+                    sx={{ width: "100%" }}
+                  >
+                    {sheetNames.map((sheetName) => (
+                      <MenuItem key={sheetName} value={sheetName}>
+                        {sheetName}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
               </Box>
             </Box>
           )}
@@ -228,8 +234,8 @@ const FlatFileUpload = ({
             <Box
               sx={{
                 position: "absolute",
-                width: 'calc(100% - 2rem);',
-                bottom: '1rem',
+                width: "calc(100% - 2rem);",
+                bottom: "1rem",
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "flex-end",
@@ -244,18 +250,17 @@ const FlatFileUpload = ({
                 hoverColor="secondary.contrastText"
                 hoverBackgroundColor="red"
                 sx={{
-                  border:'1px solid red'
+                  border: "1px solid red",
                 }}
-                onClick={() => {setSheetChooseDialogShow(false)
-                  setSelectedFile(undefined)
-                  setSheetError({isError:false,message:''})
-                  setSelectedSheet('')
-                }
-
-                }
+                onClick={() => {
+                  setSheetChooseDialogShow(false);
+                  setSelectedFile(undefined);
+                  setSheetError({ isError: false, message: "" });
+                  setSelectedSheet("");
+                }}
               />
               <HoverButton
-              disabled={sheetError.isError}
+                disabled={sheetError.isError}
                 text="Proceed"
                 color="primary.main"
                 transitionTime="0.2s"
@@ -263,7 +268,7 @@ const FlatFileUpload = ({
                 hoverColor="secondary.contrastText"
                 hoverBackgroundColor="primary.main"
                 sx={{
-                  border:'1px solid '+palette.primary.main,
+                  border: "1px solid " + palette.primary.main,
                   cursor: sheetError.isError ? "not-allowed" : "pointer",
                 }}
                 onClick={() => {
@@ -276,7 +281,8 @@ const FlatFileUpload = ({
         </Box>
       </Dialog>
     );
-    };
+  };
+
   // function to handle file select
   const handleFileSelect = (file: File | undefined) => {
     if (file) {
@@ -298,16 +304,15 @@ const FlatFileUpload = ({
         );
         setErrorDialogOpen(true);
         setSelectedFile(undefined); // Reset selected file
-        // setSelectedSheetName(""); // Reset sheet name
+        setSelectedSheetName(""); // Reset sheet name
         return;
       }
 
-      const maxFileSize = MAX_FILE_SIZE_MB * 1024 * 1024; 
-      if (file.size > maxFileSize) {
-        setErrorDialogMessage(`File size exceeds the maximum limit of ${MAX_FILE_SIZE_MB} MB.`);
+      if (file.size > MAX_FILE_SIZE_MB * 1024 * 1024) {
+        setErrorDialogMessage(`File size exceeds the maximum limit.`);
         setErrorDialogOpen(true);
         setSelectedFile(undefined);
-        // setSelectedSheetName("");
+        setSelectedSheetName("");
         return;
       }
     }
@@ -344,7 +349,7 @@ const FlatFileUpload = ({
       if (res.status) {
         const _res: TExcelDetails = res.data;
         setSheetNames(_res.sheetNames);
-        setFileInfo({fileId:_res.fileId,fileName:_res.fileName});
+        setFileInfo({ fileId: _res.fileId, fileName: _res.fileName });
       } else {
         setSheetError({
           isError: true,
@@ -375,7 +380,7 @@ const FlatFileUpload = ({
         var result: any = await FetchData({
           requestType: "withData",
           method: "POST",
-          url: `file-upload?filetype=${selectedFileType}`,
+          url: "file-upload",
           data: formData,
           headers: {
             Authorization: `Bearer ${token}`,
@@ -396,7 +401,8 @@ const FlatFileUpload = ({
           const errorMessage = result.data?.message ?? "Error uploading the file";
           setErrorDialogMessage(errorMessage);
           setErrorDialogOpen(true);
-          setSelectedFile(undefined);
+          setSelectedFile(undefined); // reset selectedFile after error
+          // setSelectedSheetName("");
         }
       }
     };
@@ -411,11 +417,11 @@ const FlatFileUpload = ({
   return (
     <div>
       <MenuBar from="fileUpload" />
-      <SheetDialog 
-      setSheetChooseDialogShow={setSheetChooseDialogShow}
-      sheetChooseDialogShow={sheetChooseDialogShow}
-      // selectedSheetName={selectedSheetName}
-      // setSelectedSheetName={setSelectedSheetName}
+      <SheetDialog
+        setSheetChooseDialogShow={setSheetChooseDialogShow}
+        sheetChooseDialogShow={sheetChooseDialogShow}
+        // selectedSheetName={selectedSheetName}
+        // setSelectedSheetName={setSelectedSheetName}
       />
       <div style={{ display: "flex" }}>
         <div
@@ -525,21 +531,7 @@ const FlatFileUpload = ({
               selectedFile={selectedFile}
               fileType={selectedFileType}
             />
-            {/* <div style={{ height: "60px" }}>
-              {selectedFileType === "excel" && (
-                <div className="excelContainer">
-                  <label className="sheetNameLabel" style={{fontSize:fontSize.medium,color:palette.primary.contrastText}}>ENTER SHEET NAME:</label>
-                  <input
-                    className="sheetNameInput"
-                    placeholder="Sheet Name"
-                    type="text"
-                    value={selectedSheetName}
-                    style={{fontSize:fontSize.medium,color:palette.primary.contrastText}}
-                    onChange={(e) => setSelectedSheetName(e.target.value)}
-                  />
-                </div>
-              )}
-            </div> */}
+
             <div className="file-upload-button-container">
               {/* <Button
                 style={{
@@ -549,8 +541,6 @@ const FlatFileUpload = ({
                   padding: "5px 20px",
                   borderRadius: "5px",
                   height: "40px",
-                  lineHeight:'normal',
-                  fontSize:fontSize.medium,
                 }}
                 onClick={handleSubmit}
               >
@@ -581,6 +571,7 @@ const FlatFileUpload = ({
                 fontSize: fontSize.medium,
                 color: palette.primary.contrastText,
                 fontWeight: "bold",
+                alignSelf: "center"
               }}
             >
               Select a File Type
