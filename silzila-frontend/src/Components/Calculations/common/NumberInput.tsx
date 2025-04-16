6a762c3f8d157782eded184238c47fefbc86b978
Siddhant Ota <tothesip@gmail.com>
2025-04-16 16:23:23 +0530

    log debounce issue fixed

--- silzila-frontend/src/Components/Calculations/common/NumberInput.tsx
+++ silzila-frontend/src/Components/Calculations/common/NumberInput.tsx
@@ -1,43 +1,52 @@
-import React from 'react'
+import React from "react";
 
 const NumberInput = ({
-    inputValue,
-    autoFocus,
-    onKeyDown,
+  inputValue,
+  autoFocus,
+  onKeyDown,
+  onBlur,
 
-    setInputValue,
-    setInputFocused,
+  setInputValue,
+  setInputFocused,
 }: {
-    inputValue: number,
-    autoFocus: boolean,
-    onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  inputValue: number;
+  autoFocus: boolean;
+  onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  onBlur?: () => void;
 
-    setInputValue: React.Dispatch<React.SetStateAction<number>>,
-    setInputFocused: React.Dispatch<React.SetStateAction<boolean>>,
+  setInputValue: React.Dispatch<React.SetStateAction<number>>;
+  setInputFocused: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
-    return (
-        <input
-            onFocus={() => setInputFocused(true)}
-            onBlur={() => setInputFocused(false)}
-            autoFocus={autoFocus}
-            min={1}
-            style={{
-                height: "1.5rem",
-                lineHeight: "1rem",
-                width: "100%",
-                margin: "0 4px",
-                marginRight: "5px",
-                paddingRight: "5px",
-                border: 'none',
-                outline: 'none',
-                color: "rgb(128, 128, 128)"
-            }}
-            type="number"
-            value={inputValue}
-            onChange={e => { setInputValue(Number(e.target.value)) }}
-            onKeyDown={onKeyDown}
-        />
-    )
-}
+  return (
+    <input
+      onFocus={() => setInputFocused(true)}
+      onBlur={() => {
+        if (onBlur) {
+          onBlur();
+        }
+        setInputFocused(false);
+      }}
+      autoFocus={autoFocus}
+      min={1}
+      style={{
+        height: "1.5rem",
+        lineHeight: "1rem",
+        width: "100%",
+        margin: "0 4px",
+        marginRight: "5px",
+        paddingRight: "5px",
+        border: "none",
+        outline: "none",
+        color: "rgb(128, 128, 128)",
+      }}
+      type="number"
+      value={inputValue}
+      onChange={(e) => {
+        setInputValue(Number(e.target.value));
+      }}
+      onKeyDown={onKeyDown}
+    />
+  );
+};
 
-export default NumberInput
+export default NumberInput;
